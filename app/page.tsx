2e966527ec183befafb73df550089fd36b2685af
Hernan <her.cw@hotmail.com>
2023-06-01 01:26:07 -0300

    3rd

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,42 @@
 "use client"
 
+import Blog from "@/components/Blog"
 import Projects from "@/components/Projects"
 import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 import { Download } from "lucide-react"
 
 const Home = () => {
   return (
-    <main className="flex justify-center min-h-screen">
-      <section className="mt-10 flex flex-col  justify-around max-w-[80%] w-full min-h-full">
-        <div className="flex flex-col justify-center w-full">
+    <main className="relative flex justify-center min-h-screen">
+      <section className="mt-10 flex flex-col  justify-around max-w-[90%] md:max-w-[80%] w-full min-h-full">
+        <div className="flex flex-col justify-center w-full my-6 ">
           <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
             Fullstack Developer
           </h1>
-          <p className="max-w-[600px] leading-7 text-muted-foreground [&:not(:first-child)]:mt-6">
+          <p className="max-w-[600px] text-center md:text-left leading-6 mb-2 text-muted-foreground [&:not(:first-child)]:mt-7">
             Hola soy Hernan, un desarrollador web con experiencia en el front y
             el back, tambien tengo ojo para el diseño y la experiencia de
             usuario.
           </p>
-          <div className="max-w-[300px] mt-6">
-            <Button>
+          <div className="my-6 md:max-w-[150px] ">
+            <Button
+              style={{
+                width: "100%",
+              }}
+            >
               <Download className="w-4 h-4 mr-2" />
               Descargar CV
             </Button>
           </div>
         </div>
         <Projects />
+        <Separator className="my-8" />
+        <Blog />
       </section>
+      <div className="blur-3xl">
+        <div className="absolute z-[-10] top-[-10rem] w-40 bg-white/5 right-[30rem] h-[800px] rotate-45"></div>
+      </div>
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,10 @@ import { Button } from "./ui/button"
 const Header = () => {
   return (
     <div className="flex justify-center border-b h-14">
-      <div className="max-w-[80%] h-full w-full flex items-center justify-between">
+      <div className="max-w-[90%] md:max-w-[80%] h-full w-full flex items-center justify-between">
         <h4 className="font-bold tracking-tight text-md scroll-m-20">
-          Hernan Barrios Paz
+          {/* if size mobile show hbp else show hernan barrios paz */}
+          {window.innerWidth < 768 ? "HBP" : "Hernan Barrios Paz"}
         </h4>
         <nav className="flex items-center">
           <Button variant="ghost">
